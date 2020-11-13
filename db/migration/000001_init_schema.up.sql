e57a8699ddebc96e0d2885a9637a72bbae0bd5ca
alifudin-a <alifudinalfarizi@gmail.com>
2020-11-13 21:05:40 +0700

    added db migrations

---
+++ db/migration/000001_init_schema.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE tasks (
+	id serial PRIMARY KEY NOT NULL,
+	title varchar NOT NULL,
+	"description" varchar NOT NULL,
+	complete varchar NOT NULL,
+	created_at timestamp NOT NULL,
+	updated_at timestamp NOT NULL,
+	deleted_at timestamp NOT NULL
+)
